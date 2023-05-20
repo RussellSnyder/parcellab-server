09d7878a9cfffe3bef345b7fe9d4eaa554f9e308
Russell Snyder <russell@rustybass.comr>
2023-05-20 15:01:24 +0200

    scaffolding for use and tracking controllers

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,10 @@ import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { SeederModule } from './seeder/seeder.module';
 import { ConfigModule } from '@nestjs/config';
+import { UserController } from './user/user.controller';
+import { UserService } from './user/user.service';
+import { TrackingController } from './tracking/tracking.controller';
+import { TrackingService } from './tracking/tracking.service';
 
 @Module({
   imports: [
@@ -13,7 +17,7 @@ import { ConfigModule } from '@nestjs/config';
     PrismaModule,
     SeederModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, UserController, TrackingController],
+  providers: [AppService, UserService, TrackingService],
 })
 export class AppModule {}
